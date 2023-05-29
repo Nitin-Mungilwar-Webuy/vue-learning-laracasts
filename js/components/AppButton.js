4746eb13b2226fd3005c9ac3380ff2e833246bf4
Nitin <132347798+nitin8857888@users.noreply.github.com>
2023-05-29 12:15:19 +0530

    without passing props

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -2,6 +2,9 @@ export default {
   template: `<button class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed"  :disabled="complete">
   <slot></slot>
 </button>`,
+  props: {
+    type: String,
+  },
   data() {
     return {
       complete: true,
