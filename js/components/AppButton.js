183f2511819caf62faa1a113e90ca1e39d2548bd
Nitin <132347798+nitin8857888@users.noreply.github.com>
2023-05-29 12:33:50 +0530

    dynamic class according to props

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,5 +1,10 @@
 export default {
-  template: `<button class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed"  :disabled="complete">
+  template: `<button :class="{
+    'border rounded px-5 py-2 disabled:cursor-not-allowed':true,
+    'bg-blue-200 hover:bg-blue-400':type==='primary',
+    'bg-green-200 hover:bg-green-400':type==='secondary',
+    'bg-red-200 hover:bg-red-400':type==='muted',
+  }">
   <slot></slot>
 </button>`,
   props: {
